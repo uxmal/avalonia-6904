6f28ba48108b1cca0dfad70e9f92509ccbcbcc45
Tim <tim.uniewski@gmail.com>
2021-11-10 09:41:12 +0100

    Doing expensive work async

--- EagerEnumeration/ViewModels/ExpensiveObject.cs
+++ EagerEnumeration/ViewModels/ExpensiveObject.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace EagerEnumeration.ViewModels
 {
@@ -8,32 +11,57 @@ namespace EagerEnumeration.ViewModels
     /// This view model class was expensive to compute/obtain. We want these to be 
     /// created only as necessary.
     /// </summary>
-    public class ExpensiveObject
+    public class ExpensiveObject : INotifyPropertyChanged
     {
-        public ExpensiveObject()
+        private int _Number;
+        public ExpensiveObject(int n)
         {
+            this._Number = n;
             // Simulate these objects being expensive to compute.
-            Thread.Sleep(500);
+
+            // We should do this in a different property.
+            // Thread.Sleep(500);
         }
 
-        public string Name { get; set; } = "";
-        public string Body { get; set; } = "";
+        private string _Name = "Loading ...";
 
-        public static ExpensiveObject Create(int n)
+        /// <summary>
+        /// comment
+        /// </summary>
+        public string Name
         {
+            get => _Name;
+            private set
+            {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Task<string> AsyncBody => GetBodyAsync();
+
+        private async Task<string> GetBodyAsync()
+        {
+            await Task.Delay(500);
+            
             var rnd = new Random();
             int Rnd(int m) => rnd.Next() % m;
             var nLines = Rnd(6) + Rnd(6) + Rnd(6) + 1;
-            var name = $"Name{n}";
             var body = string.Join(Environment.NewLine,
                 Enumerable.Range(0, nLines)
                     .Select(u => $"  Line {u}"));
-            return new ExpensiveObject
-            {
-                Name = name,
-                Body = body,
-            };
+            
+            Name = $"Name{_Number}";
+            
+            return body;
         }
 
+        // We will use Property changed event to update our Name-Property after the content is computed.
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
